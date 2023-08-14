9bdbb831cdd2703975f7904bc693b7b3e4694e6d
MaxjoseC <Maxjose17@gmail.com>
2023-08-13 22:28:23 -0400

    UI completa servicio
    UI completa servicio

--- src/main/java/view/ServicioFrame.java
+++ src/main/java/view/ServicioFrame.java
@@ -20,11 +20,16 @@ import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import java.awt.Dimension;
+import javax.swing.JSpinner;
 
 public class ServicioFrame extends JInternalFrame {
 	private JTextField txtservicioID;
 	private JTextField txtDescripcionS;
 	private JTable tableS;
+	private JTextField txtatencionID;
+	private JTextField txtPrecio;
+	private JTable table;
 
 
 	/**
@@ -33,7 +38,7 @@ public class ServicioFrame extends JInternalFrame {
 	public ServicioFrame() {
 		setTitle("Servicios");
 		setClosable(true);
-		setBounds(100, 100, 600, 700);
+		setBounds(100, 100, 841, 654);
 		
 		JPanel Pnelservicios = new JPanel();
 		Pnelservicios.setBorder(new TitledBorder(null, "Servicios", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -101,7 +106,7 @@ public class ServicioFrame extends JInternalFrame {
 		});
 		pnlSacciones.add(btnnuevoS);
 		
-		JButton btnguardarservicio = new JButton("Guardar");
+		JButton btnguardarservicio = new JButton("Agregar");
 		pnlSacciones.add(btnguardarservicio);
 		
 		JButton btnNuevo = new JButton("Actualizar");
@@ -118,7 +123,11 @@ public class ServicioFrame extends JInternalFrame {
 		});
 		pnlSacciones.add(btnBuscarS);
 		
-		JButton btnBorrar = new JButton("Borrar");
+		JButton btnBorrar = new JButton("Eliminar");
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		pnlSacciones.add(btnBorrar);
 		
 		JPanel pnRegistro = new JPanel();
@@ -127,9 +136,12 @@ public class ServicioFrame extends JInternalFrame {
 		pnRegistro.setLayout(new BorderLayout(0, 0));
 		
 		JScrollPane scrollPnServicioR = new JScrollPane();
+		scrollPnServicioR.setMaximumSize(new Dimension(600, 700));
+		scrollPnServicioR.setSize(new Dimension(600, 600));
 		pnRegistro.add(scrollPnServicioR, BorderLayout.CENTER);
 		
 		tableS = new JTable();
+		tableS.setRowHeight(8);
 		tableS.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
@@ -145,11 +157,101 @@ public class ServicioFrame extends JInternalFrame {
 			}
 		});
 		scrollPnServicioR.setViewportView(tableS);
+		tableS.setSize(200, 200);
+		tableS.setPreferredScrollableViewportSize(new Dimension(600, 80));
+		JPanel pnAtenciones = new JPanel();
+		pnAtenciones.setBorder(new TitledBorder(null, "Atenciones", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnRegistro.add(pnAtenciones, BorderLayout.SOUTH);
+		pnAtenciones.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Atenciones", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		pnRegistro.add(panel, BorderLayout.SOUTH);
-		panel.setLayout(new BorderLayout(0, 0));
+		JPanel pndatosA = new JPanel();
+		pndatosA.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnAtenciones.add(pndatosA, BorderLayout.CENTER);
+		pndatosA.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblidAten = new JLabel("ID:");
+		pndatosA.add(lblidAten, "2, 2");
+		
+		txtatencionID = new JTextField();
+		pndatosA.add(txtatencionID, "14, 2, fill, default");
+		txtatencionID.setColumns(10);
+		
+		JLabel lblAtencionCantidad = new JLabel("Cantidad:");
+		pndatosA.add(lblAtencionCantidad, "2, 4");
+		
+		JSpinner spinner = new JSpinner();
+		pndatosA.add(spinner, "14, 4");
+		
+		JLabel lblPrecioA = new JLabel("Precio:");
+		pndatosA.add(lblPrecioA, "2, 6");
+		
+		txtPrecio = new JTextField();
+		pndatosA.add(txtPrecio, "14, 6, fill, default");
+		txtPrecio.setColumns(10);
+		
+		JLabel lblnuevo = new JLabel("Tipo");
+		pndatosA.add(lblnuevo, "2, 8");
+		
+		JComboBox cmbTipoAten = new JComboBox();
+		pndatosA.add(cmbTipoAten, "14, 8, fill, default");
+		
+		JButton btnAtenAgregar = new JButton("Agregar");
+		btnAtenAgregar.addActionListener(e -> {
+			
+        });
+		pndatosA.add(btnAtenAgregar, "14, 10");
+		
+		JPanel pnRegistroA = new JPanel();
+		pnRegistroA.setBorder(new TitledBorder(null, "Registros", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnAtenciones.add(pnRegistroA, BorderLayout.SOUTH);
+		pnRegistroA.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrpnRegistroA = new JScrollPane();
+		pnRegistroA.add(scrpnRegistroA, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID", "Precio", "Cantidad", "Tipo"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrpnRegistroA.setViewportView(table);
+		table.setSize(200, 200);
+		table.setPreferredScrollableViewportSize(new Dimension(600, 80));
 
 	}
 
